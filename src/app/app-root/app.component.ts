f239b332045e223a5c604af66d02fd98db6d67fb
Moria Hamami <moriahamami@gmail.com>
2023-02-22 16:17:21 +0200

    Remove readme file

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'contacts';
-
-    selectedCmp = 'signup';
-
-    selectCmp(cmpName: string) {
-        this.selectedCmp = cmpName;
-    }
 
 }
--- src/app/pages/contact-edit/contact-edit.component.ts
+++ src/app/pages/contact-edit/contact-edit.component.ts
@@ -20,7 +20,6 @@ export class ContactEditComponent implements OnInit {
 
 
     ngOnInit(): void {
-
         this.subscription = this.route.data.subscribe(({ contact }) => {
             this.contact = contact || this.contactService.getEmptyContact() as Contact
         })
@@ -63,3 +62,86 @@ export class ContactEditComponent implements OnInit {
     }
 
 }
+
+
+
+
+
+
+
+
+// import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Subscription } from 'rxjs';
+// import { Contact } from 'src/app/models/contact.model';
+// import { ContactService } from 'src/app/services/contact.service';
+// import { faTrash } from '@fortawesome/free-solid-svg-icons';
+// import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+
+
+// @Component({
+//     selector: 'contact-edit',
+//     templateUrl: './contact-edit.component.html',
+//     styleUrls: ['./contact-edit.component.scss']
+// })
+// export class ContactEditComponent implements OnInit, OnDestroy {
+
+//     form !: FormGroup
+//     contact: Contact | undefined
+//     subscription!: Subscription
+//     faTrash = faTrash
+//     faCircleArrowLeft = faCircleArrowLeft
+
+//     constructor(
+//         private contactService: ContactService,
+//         private route: ActivatedRoute,
+//         private router: Router,
+//         private fb: FormBuilder) {
+//         this.form = this.fb.group({
+//             name: ['', [Validators.required]],
+//             email: '',
+//             phone: ''
+//         })
+//     }
+
+//     ngOnInit(): void {
+//         this.subscription = this.route.data.subscribe(({ contact }) => {
+//             if (contact) {
+//                 this.contact = contact
+//                 this.form.patchValue(contact)
+//             }
+//         })
+
+//     }
+
+//     onSubmit(): void {
+//         try {
+//             const contact = { ...this.contact, ...this.form.value }
+//             this.contactService.saveContact(contact)
+//             this.onBack()
+//         } catch (err) {
+//             console.error(err)
+//         }
+//     }
+
+//     onRemove() {
+//         try {
+//             if (this.contact?._id) this.contactService.deleteContact(this.contact?._id)
+//             this.onBack()
+//         } catch (err) {
+//             console.error(err)
+//         }
+//     }
+
+//     onBack(): void {
+//         this.router.navigateByUrl('/contact')
+//     }
+
+//     ngOnDestroy(): void {
+//         this.subscription.unsubscribe()
+//     }
+// }
+
+
